0d89ded1546e761ca8f0d69dba06eded9b2d9e17
Pablo Ruiz Uroz <lttpablo@gmail.com>
2025-10-12 18:50:52 +0200

    todoService basic fucntions

--- src/toDoRouter.js
+++ src/toDoRouter.js
@@ -13,6 +13,18 @@ const upload = multer({ dest: UPLOADS_FOLDER });
 router.get("/", (req, res) => {
   res.render("index");
   });
+
+router.post("/task/add",(req,res) => {
+
+  let task = {
+    title: req.body.title,
+    description: req.body.description,
+    dueDate: req.body.dueDate,
+    priority: req.body.priority,
+  }
+  toDoService.addTask(task);
+  res.render("index");
+})
   
 
 
--- src/toDoService.js
+++ src/toDoService.js
@@ -1,4 +1,22 @@
 const tasks = new Map();
 let nextId = 0;
 
+export function addTask(task) {
+    let id = nextId++;
+    task.id = id.toString();
+    task.completed = false;
+    tasks.set(task.id, task);
+}
 
+export function deleteTask(id) {
+    let task = getTask(id);
+    tasks.delete(id);
+    return task;
+}
+
+export function getTasks() {
+    return [...tasks.values()];
+}
+export function getTask(id) {
+    return tasks.get(id);
+}
